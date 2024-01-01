166fedf554268c261907d23686d8dd88177007fe
a98c14 <selimyesilkaya@gmail.com>
2024-01-01 16:37:37 +0300

    formatting

--- src/physics/intersection.c
+++ src/physics/intersection.c
@@ -5,12 +5,12 @@ intersects_rect_point(Rect a, Vec2 b)
 {
     Vec2 bl = rect_bl(a);
     Vec2 tr = rect_tr(a);
-    
+
     return !(b.x < bl.x || b.y < bl.y || b.x > tr.x || b.y > tr.y);
 }
 
 internal bool32
 intersects_circle_point(Circle a, Vec2 b)
 {
-    return lensqr_vec2(sub_vec2(b, a.center)) < (a.radius/2 * a.radius/2);
+    return lensqr_vec2(sub_vec2(b, a.center)) < (a.radius / 2 * a.radius / 2);
 }
--- src/physics/intersection.h
+++ src/physics/intersection.h
@@ -4,5 +4,4 @@
 #include <core/math.h>
 
 internal bool32 intersects_rect_point(Rect a, Vec2 b);
-
 internal bool32 intersects_circle_point(Circle a, Vec2 b);
