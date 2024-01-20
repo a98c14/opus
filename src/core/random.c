c56112aee0b046156ae95019e6ff92b883cca253
a98c14 <selimyesilkaya@gmail.com>
2024-01-21 00:02:04 +0300

    add random point in circle function

--- src/core/random.c
+++ src/core/random.c
@@ -1,4 +1,5 @@
 #include "random.h"
+#include <core/math.h>
 #include <pcg/pcg_basic.c>
 
 internal void
@@ -19,4 +20,13 @@ random_f32(float32 max)
 {
     const float32 resolution = 8192.0f;
     return pcg32_boundedrand_r(&g_rng, resolution * max) / resolution;
+}
+
+internal Vec2
+random_point_in_circle(Vec2 center, float32 radius)
+{
+    float32 angle = random_between_f32(-180, 180);
+    float32 scale = random_between_f32(0, radius);
+    Vec2    v     = direction_vec2(angle, scale);
+    return add_vec2(center, v);
 }
--- src/core/random.h
+++ src/core/random.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "defines.h"
+#include "math.h"
 #include <pcg/pcg_basic.h>
 
 global pcg32_random_t g_rng;
@@ -8,3 +9,4 @@ global pcg32_random_t g_rng;
 internal void    random_init(uint64 seed);
 internal float32 random_between_f32(float32 min, float32 max);
 internal float32 random_f32(float32 max);
+internal Vec2    random_point_in_circle(Vec2 center, float32 radius);
